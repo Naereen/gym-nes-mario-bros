efbae5215bdd2e3e32a249e437d019099c988911
Lilian Besson <lbesson@ens-cachan.fr>
2018-02-24 05:19:21 +0100

    Done for tonight. Fun night

--- src/run-mario.py
+++ src/run-mario.py
@@ -23,7 +23,7 @@ def mario_main():
 
     last_obs = env.reset()
 
-    max_timesteps = 40000000
+    max_timesteps = 4000000
 
     exploration_schedule = PiecewiseSchedule(
         [
@@ -35,12 +35,15 @@ def mario_main():
 
     dqn = DoubleDQN(image_shape=(84, 84, 1),
                     num_actions=env.action_space.n,
-                    training_starts=10000,
-                    target_update_freq=4000,
-                    training_batch_size=64,
-                    # training_starts=1000,
-                    # target_update_freq=500,
-                    # training_batch_size=3,
+                    # XXX Heavy simulations
+                    # training_starts=10000,
+                    # target_update_freq=4000,
+                    # training_batch_size=64,
+                    # XXX light simulations?
+                    training_starts=1000,
+                    target_update_freq=500,
+                    training_batch_size=3,
+                    # Other parameters
                     frame_history_len=4,
                     replay_buffer_size=100000,  # XXX reduce if MemoryError
                     exploration=exploration_schedule
@@ -50,7 +53,7 @@ def mario_main():
     num_episodes = 0
     episode_rewards = deque(maxlen=100)
     for step in range(max_timesteps):
-        if step > 0 and step % 500 == 0:
+        if step > 0 and step % 50 == 0:
             print('step: ', step, 'episodes:', num_episodes, 'epsilon:', exploration_schedule.value(step),
                   'learning rate:', dqn.get_learning_rate(), 'last 100 training loss mean', dqn.get_avg_loss(),
                   'last 100 episode mean rewards: ', np.mean(np.array(episode_rewards, dtype=np.float32)))
